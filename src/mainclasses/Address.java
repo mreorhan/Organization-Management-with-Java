8598401411cb709b7cce297aaa87253269e9235f
Emre Orhan <videokod7@gmail.com>
2018-10-22 23:49:55 +0300

    Added company & adress classes

---
+++ src/mainclasses/Address.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainclasses;
+
+/**
+ *
+ * @author Emre
+ */
+public class Address {
+    private int postCode;
+    private String country;
+    private String province;
+    private String street;
+}
---
+++ src/mainclasses/Company.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainclasses;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Emre
+ */
+public class Company {
+    private String companyName;
+    private Date companyCreatedDate;
+    private String companyVision;
+    private String companyMission;
+    private Address compAddress;
+}
