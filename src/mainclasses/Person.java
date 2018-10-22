797cb4e9856496e408f8fc13e3a5a8fd02dac8d4
Emre Orhan <videokod7@gmail.com>
2018-10-22 23:33:37 +0300

    Initialized files

---
+++ src/mainclasses/Person.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainclasses;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Emre
+ */
+public class Person {
+    
+    private String name;
+    private String lastName;
+    private int personID;
+    private Date birthdayDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(int personID) {
+        this.personID = personID;
+    }
+
+    public Date getBirthdayDate() {
+        return birthdayDate;
+    }
+
+    public void setBirthdayDate(Date birthdayDate) {
+        this.birthdayDate = birthdayDate;
+    }
+    
+}
