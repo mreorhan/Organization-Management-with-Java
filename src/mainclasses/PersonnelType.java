e33706404d198bfb5653bdc1c2ef0489225405d5
Emre Orhan <videokod7@gmail.com>
2018-10-22 23:33:37 +0300

    Initialized files

---
+++ src/mainclasses/PersonnelType.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainclasses;
+
+import javax.annotation.Resource;
+
+/**
+ *
+ * @author Emre
+ */
+public enum PersonnelType {
+    SoftwareEngineer,
+    BofD,
+    ProjectLeader,
+    Employee,
+    HumanResourcest,
+    Accountant
+}
