25db4310f95caba31ba89b3574f1c7dcad5ba44f
Emre Orhan <videokod7@gmail.com>
2018-10-24 12:19:43 +0300

    Added SignUp Screen

--- src/organization/management/FXMLDocumentController.java
+++ src/organization/management/FXMLDocumentController.java
@@ -12,17 +12,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
  * @author OGUZHAN
  */
 public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private AnchorPane rootPane;
     
     @FXML
     private JFXPasswordField txt_password;
@@ -32,6 +38,8 @@ public class FXMLDocumentController implements Initializable {
     private JFXButton btn_fotgotpassword;
     @FXML
     private JFXButton btn_login;
+    @FXML
+    private Label lbl_register;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -42,6 +50,13 @@ public class FXMLDocumentController implements Initializable {
         String username = txt_username.getText();
         String password = txt_password.getText();
         System.out.println(username +","+password);
+    }
+
+    @FXML
+    private void signUpClick(MouseEvent event)  throws  Exception{
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("SignUp_screen.fxml"));
+        rootPane.getChildren().setAll(pane);
+        System.err.println("ok");
     }
 
     
