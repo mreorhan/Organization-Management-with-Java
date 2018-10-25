dc7cc214276fe56bbec92ee110d9f63ebf73e72d
Emre Orhan <videokod7@gmail.com>
2018-10-26 02:05:25 +0300

    Updated Dashboard Screen

--- src/organization/management/Dashboard_ScreenController.java
+++ src/organization/management/Dashboard_ScreenController.java
@@ -7,6 +7,8 @@ package organization.management;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -23,5 +25,11 @@ public class Dashboard_ScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void onLogoutAction(ActionEvent event) {
+        CommonFunction fo = new CommonFunction();
+        fo._show("SignIn_Screen.fxml");
+    }
     
 }
