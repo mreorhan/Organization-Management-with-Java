8facfdf031383a9ad09ba93369b1396f41817b04
Oğuzhan <30536489+oguzhandeveloper@users.noreply.github.com>
2018-10-27 19:47:26 +0300

    Fixed jobId Access

--- src/organization/management/SignIn_ScreenController.java
+++ src/organization/management/SignIn_ScreenController.java
@@ -31,12 +31,13 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
+
 /**
  *
  * @author OGUZHAN
  */
 public class SignIn_ScreenController implements Initializable {
-    
+
     @FXML
     private JFXPasswordField txt_password;
     @FXML
@@ -49,36 +50,56 @@ public class SignIn_ScreenController implements Initializable {
     private JFXButton btn_login;
     @FXML
     private AnchorPane panel;
-    
-    private CommonFunction fo=new CommonFunction();
+
+    private CommonFunction fo = new CommonFunction();
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-	
+
     }
+
     @FXML
     private void loginClickAction(ActionEvent event) {
         String username = txt_username.getText();
         String password = txt_password.getText();
-        
-        DBHelper db= new DBHelper();
+
+        DBHelper db = new DBHelper();
         db.open();
         Person user = db.UserControl(username, password);
-        InstantData.person=user;
-        
-        
-        
-         if(user!=null){
-            //Yeni formu açmak için kullanıyoruz
-            fo._show("Dashboard_Screen.fxml",panel);
+        InstantData.person = user;
+
+        String[][] jobType = db.SelectFromTable("jobtype");
+        String[][] personel = db.SelectFromTable("personnel");
+
+        String job = "";
+        String  jobID = "";
+
+        for (int i = 0; i < personel.length; i++) {
+            if (personel[i][0].equals(user.getPersonID()+"")) {
+                for (int j = 0; j < jobType.length; j++) {
+                    if (personel[i][2].equals(jobType[j][0])) {
+                        job = jobType[j][1];
+                        jobID = jobType[j][0];
+                        break;
+                    }
+                }
+            }
         }
-        else {
-             fo._modal("Error","Username or password is wrong. Please try again.","OK",panel);
+        System.out.println("JobID: "+jobID+" Job: "+job);
+        InstantData.personJobName=job;
+
+        if (user != null) {
+            //Yeni formu açmak için kullanıyoruz
+            fo._show("Dashboard_Screen.fxml", panel);
+        } else {
+            fo._modal("Error", "Username or password is wrong. Please try again.", "OK", panel);
         }
         db.close();
     }
+
     @FXML
     private void signUpClick(MouseEvent event) {
-        fo._show("SignUp_Screen.fxml",panel);
+        fo._show("SignUp_Screen.fxml", panel);
     }
-    
+
 }
